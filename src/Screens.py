945d70d0b90af6689a58cdd85f507ad8f2176768
Noah Ruben <noah.ruben@online.de>
2018-06-01 16:47:14 +0200

    Clean UP!

--- src/Screens.py
+++ src/Screens.py
@@ -96,7 +96,8 @@ class InGameMenu(tkinter.Frame):
         self.game.close()
 
     def options(self):
-        # TODO get_config_parser the Options Screen
+        #TODO get_config_parser the Options Screen
+        pass
 
     def close(self):
         self.is_active = False
