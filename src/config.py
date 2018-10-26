c63adf431080225c1eb1cd2e613078fb6c2a9e5f
Noah Ruben <noah.ruben@online.de>
2018-10-26 21:40:12 +0200

    Refactor of config.py

--- src/config.py
+++ src/config.py
@@ -1,25 +1,36 @@
+"""
+This module is used handle the config-file of the game
+
+Import
+------
+    os
+        used to get the config-file destination
+
+    ConfigParser
+        used to get easy access to the config-file
+"""
+
 import os
-import configparser
+from configparser import ConfigParser
 
-sound_section = "game.sound"
+sound_section: str = "game.sound"
+music_volume_option: str = "music_volume"
+music_on_option: str = "music_on"
+sfx_volume_option: str = "sfx_volume"
+sfx_on_option: str = "sfx_on"
 
-music_volume_option = "music_volume"
-music_on_option = "music_on"
-sfx_volume_option = "sfx_volume"
-sfx_on_option = "sfx_on"
-
-parent_dir = os.path.dirname(os.getcwd())
-file_path = os.path.join(parent_dir, "config.cfg")
+parent_dir: str = os.path.dirname(os.getcwd())
+file_path: str = os.path.join(parent_dir, "config.cfg")
 
 
-def set_value(config_section: str, config_to_set: str, value: str):
-    config_parser = configparser.ConfigParser()
+def set_value(config_section: str, config_to_set: str, value: str) -> None:
+    config_parser: ConfigParser = ConfigParser()
     config_parser.read(file_path)
     config_parser.set(config_section, config_to_set, value)
     config_parser.write(open(file_path, 'w'))
 
 
-def get_value(config_section: str, config_to_set: str):
-    config_parser = configparser.ConfigParser()
+def get_value(config_section: str, config_to_set: str) -> str:
+    config_parser: ConfigParser = ConfigParser()
     config_parser.read(file_path)
     return config_parser.get(config_section, config_to_set)
